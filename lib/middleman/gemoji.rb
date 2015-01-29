0d062666011fb1e07be47009d176874013a247ef
yterajima <terra@e2esound.com>
2015-01-29 14:18:01 +0900

    first commit

---
+++ lib/middleman/gemoji.rb
@@ -0,0 +1,7 @@
+require "middleman/gemoji/version"
+
+module Middleman
+  module Gemoji
+    # Your code goes here...
+  end
+end
---
+++ lib/middleman/gemoji/version.rb
@@ -0,0 +1,5 @@
+module Middleman
+  module Gemoji
+    VERSION = "0.0.1"
+  end
+end
