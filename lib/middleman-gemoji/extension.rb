dbe8a6b1de3f852af8cf22c60566b771c3c49edf
yterajima <terra@e2esound.com>
2015-01-31 21:23:46 +0900

    Fix code using Rubocop

--- lib/middleman-gemoji/extension.rb
+++ lib/middleman-gemoji/extension.rb
@@ -1,16 +1,19 @@
-require "gemoji"
+require 'gemoji'
 
 module Middleman
   module Gemoji
+    # Emoji Convert Extension of Middleman
     class Extension < ::Middleman::Extension
-      option :size, nil, "Size(width/height) of emoji im img tag"
-      option :style, nil, "Value of style attribute"
-      option :emoji_dir, "images/emoji", "Path to Emoji directory from source"
+      option :size, nil, 'Size(width/height) of emoji im img tag'
+      option :style, nil, 'Value of style attribute'
+      option :emoji_dir, 'images/emoji', 'Path to Emoji directory from source'
 
       def initialize(app, options_hash = {}, &block)
         super
-        @emoji_exist = Dir.exist?(File.join(app.config.source, options[:emoji_dir], "unicode"))
-        extension = self
+        @emoji_exist = Dir.exist?(
+          File.join(app.config.source, options[:emoji_dir], 'unicode')
+        )
+        extension    = self
 
         app.after_render do |content|
           extension.emojify(content)
@@ -20,14 +23,15 @@ module Middleman
       def emojify(content)
         @base_path = base_path
         content.to_str.gsub(/:([\w+-]+):/) do |match|
-          if emoji = Emoji.find_by_alias($1)
+          emoji = Emoji.find_by_alias($1)
+          if emoji
             image = []
             image << %(alt="#{$1}")
             image << src(emoji.image_filename)
             image << size(options[:size]) if options[:size]
             image << style(options[:style]) if options[:style]
 
-            "<img #{image.join(" ")} />"
+            "<img #{image.join(' ')} />"
           else
             match
           end if content.present?
@@ -39,11 +43,11 @@ module Middleman
       end
 
       def size(size)
-        %(width="#{size.to_s}" height="#{size.to_s}")
+        %(width="#{size}" height="#{size}")
       end
 
       def style(style)
-        %(style="#{style.to_s}")
+        %(style="#{style}")
       end
 
       def base_path
@@ -52,10 +56,10 @@ module Middleman
           if prefix && (/^http/ =~ prefix)
             File.join(app.config[:http_prefix], options[:emoji_dir])
           else
-            File.join("/", app.config[:http_prefix], options[:emoji_dir])
+            File.join('/', app.config[:http_prefix], options[:emoji_dir])
           end
         else
-          "https://assets-cdn.github.com/images/icons/emoji/"
+          'https://assets-cdn.github.com/images/icons/emoji/'
         end
       end
     end
--- lib/middleman-gemoji/version.rb
+++ lib/middleman-gemoji/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   module Gemoji
-    VERSION = "0.0.2"
+    VERSION = '0.0.2'
   end
 end
